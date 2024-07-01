03d42e7ac4071602410c788fe790bdbace900c81
RomanJelinek <roman.jelinek91@gmail.com>
2024-07-01 07:52:44 +0200

    fix: fixed type for the client

--- src/libs/supabase/supabase.server.ts
+++ src/libs/supabase/supabase.server.ts
@@ -1,11 +1,10 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
-import { Product } from 'puppeteer';
 
- const createClient = () => {
+const createClient = () => {
   const cookieStore = cookies();
 
-  return createServerClient<Product>(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
