116fe2784789841f184be153ea10c8dd06df5a91
RomanJelinek <roman.jelinek91@gmail.com>
2024-07-04 08:32:43 +0200

    feat: added CategoryCard and implemented

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,16 +1,29 @@
 import ProductList from '@/components/ProductList';
 import { getCategory } from '../utility/categories';
+import CategoryCard from '@/components/CategoryCard';
 
 export default async function Category({
   params: { slug },
 }: {
   params: { slug: string };
 }) {
-  const { category } = getCategory(slug);
+  const { category, subtype } = getCategory(slug);
+  const chosenCategory = subtype || category;
   if (!category) return <>Kategorie nenalezena</>;
   return (
     <main>
-      <ProductList filters={{ type: category.type }} />
+      <div className="container mx-auto p-6 text-center">
+        <h1 className="text-3xl font-bold mb-4">{chosenCategory.headline}</h1>
+        <p className="text-gray-600 mb-12">{chosenCategory.description}</p>
+        <div className="flex gap-2 mb-12 justify-center flex-wrap">
+          {category.subtypes.map((sub) => (
+            <CategoryCard title={sub.headline} link={sub.url} />
+          ))}
+        </div>
+        <ProductList
+          filters={{ subtype: subtype?.subtype, type: category.type }}
+        />
+      </div>
     </main>
   );
 }
---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface CategoryCardProps {
+  title: string;
+  link: string;
+}
+
+const CategoryCard = ({ title, link }: CategoryCardProps) => {
+  return (
+    <a
+      href={link}
+      className="block p-2 bg-white border border-gray-300 rounded-md hover:shadow-lg transition-shadow duration-300"
+    >
+      <div className="flex items-center justify-center h-8">
+        <span className="text-sm font-semibold text-center">{title}</span>
+      </div>
+    </a>
+  );
+};
+
+export default CategoryCard;
