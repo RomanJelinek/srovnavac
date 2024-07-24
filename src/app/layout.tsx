b02a75501d19c55571b36d6c9918e0bc9aa85414
RomanJelinek <roman.jelinek91@gmail.com>
2024-07-24 18:23:21 +0200

    added canonical globaly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Co dělat v Praze? Máme pro vás tipy na Zážitky v Praze",
+  metadataBase: new URL(process.env.WEB_URL),
+  title: "Co dělat v Praze? Skvělé tipy na Zážitky Praha",
   description:
     "Objevte nejlepší aktivity v Praze! Průvodce historií, kulturou, gastronomií a zábavou. Najděte ty nejlepší tipy na zážitky v tomto kouzelném městě.",
   alternates: {
