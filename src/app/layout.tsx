e6138a67c507e7063e000782a7d641c882579bcf
RomanJelinek <roman.jelinek91@gmail.com>
2024-07-24 19:33:43 +0200

    temporary meta for verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: "./",
   },
+  other: {
+    "google-site-verification": "3Nd7Utfql33iA-ffbLFqal4t2QqrhIifGq7TAa60Yfg",
+  },
 };
 
 export default function RootLayout({
